f498474f7abb572cd07be477a50805cf1e37c5c0
Robin Lake <robinhlake@gmail.com>
2023-05-01 12:22:32 -0700

    switch to react

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+  let audioContext = new AudioContext()
+  return (
+    <>
+      <h1>Placeholder</h1>
+
+    </>
+  )
+}
+
+export default App
